debc8270c0ce542f275911029cf16c402fd2c67d
Dishan Ranasinghe <162302275+RanasingheDD@users.noreply.github.com>
2025-07-06 19:49:18 +0530

    update

--- Setting.cs
+++ Setting.cs
@@ -87,5 +87,22 @@ namespace VirusGuard
         {
 
         }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            Setting setting = new Setting();
+            setting.Show();
+            ;
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
