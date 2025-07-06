32810d1da5d9d964db8657759df44ce56b8e95b7
Kushitha Lakshitha <168770766+Kushitha1521@users.noreply.github.com>
2025-07-06 23:17:26 +0530

    FAQ

--- FAQ.Designer.cs
+++ FAQ.Designer.cs
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FAQ));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             label7 = new Label();
             btnToggle1 = new Guna.UI2.WinForms.Guna2Button();
             faqPanel1 = new Guna.UI2.WinForms.Guna2Panel();
             lblAnswer1 = new Label();
-            faqTimer1 = new System.Windows.Forms.Timer(components);
+            faqPanel2 = new Guna.UI2.WinForms.Guna2Panel();
+            lblAnswer2 = new Label();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            faqPanel3 = new Guna.UI2.WinForms.Guna2Panel();
+            label1 = new Label();
+            lblAnswer3 = new Guna.UI2.WinForms.Guna2Button();
             faqPanel1.SuspendLayout();
+            faqPanel2.SuspendLayout();
+            faqPanel3.SuspendLayout();
             SuspendLayout();
             // 
             // label7
@@ -69,7 +83,7 @@
             btnToggle1.ShadowDecoration.CustomizableEdges = customizableEdges2;
             btnToggle1.Size = new Size(493, 34);
             btnToggle1.TabIndex = 24;
-            btnToggle1.Text = "1. What is Virus Guard?";
+            btnToggle1.Text = "1. What is Medusa?";
             btnToggle1.TextAlign = HorizontalAlignment.Left;
             btnToggle1.Click += btnToggle1_Click;
             // 
@@ -93,15 +107,99 @@
             lblAnswer1.Location = new Point(15, 40);
             lblAnswer1.MaximumSize = new Size(600, 0);
             lblAnswer1.Name = "lblAnswer1";
-            lblAnswer1.Size = new Size(599, 48);
+            lblAnswer1.Size = new Size(597, 48);
             lblAnswer1.TabIndex = 26;
             lblAnswer1.Text = resources.GetString("lblAnswer1.Text");
             lblAnswer1.Visible = false;
             lblAnswer1.Click += lblAnswer1_Click;
             // 
-            // faqTimer1
+            // faqPanel2
             // 
-            faqTimer1.Interval = 10;
+            faqPanel2.AutoScroll = true;
+            faqPanel2.Controls.Add(lblAnswer2);
+            faqPanel2.Controls.Add(guna2Button1);
+            faqPanel2.CustomizableEdges = customizableEdges7;
+            faqPanel2.Location = new Point(35, 186);
+            faqPanel2.Name = "faqPanel2";
+            faqPanel2.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            faqPanel2.Size = new Size(646, 68);
+            faqPanel2.TabIndex = 26;
+            // 
+            // lblAnswer2
+            // 
+            lblAnswer2.AutoSize = true;
+            lblAnswer2.Font = new Font("Lato", 9.749999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAnswer2.ForeColor = Color.White;
+            lblAnswer2.Location = new Point(15, 40);
+            lblAnswer2.MaximumSize = new Size(600, 0);
+            lblAnswer2.Name = "lblAnswer2";
+            lblAnswer2.Size = new Size(384, 16);
+            lblAnswer2.TabIndex = 26;
+            lblAnswer2.Text = "Make sure the enable real-time protection  and update in setting.";
+            lblAnswer2.Visible = false;
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.CustomizableEdges = customizableEdges5;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.Transparent;
+            guna2Button1.Font = new Font("Lato", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2Button1.ForeColor = Color.White;
+            guna2Button1.Location = new Point(0, 3);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2Button1.Size = new Size(493, 34);
+            guna2Button1.TabIndex = 24;
+            guna2Button1.Text = "2. Medusa is not detecting threats. What should I do?";
+            guna2Button1.TextAlign = HorizontalAlignment.Left;
+            guna2Button1.Click += guna2Button1_Click;
+            // 
+            // faqPanel3
+            // 
+            faqPanel3.AutoScroll = true;
+            faqPanel3.Controls.Add(label1);
+            faqPanel3.Controls.Add(lblAnswer3);
+            faqPanel3.CustomizableEdges = customizableEdges11;
+            faqPanel3.Location = new Point(35, 274);
+            faqPanel3.Name = "faqPanel3";
+            faqPanel3.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            faqPanel3.Size = new Size(646, 94);
+            faqPanel3.TabIndex = 27;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Lato", 9.749999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(15, 40);
+            label1.MaximumSize = new Size(600, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(273, 16);
+            label1.TabIndex = 26;
+            label1.Text = "Control Panel > Programs > Uninstall Medusa";
+            label1.Visible = false;
+            // 
+            // lblAnswer3
+            // 
+            lblAnswer3.CustomizableEdges = customizableEdges9;
+            lblAnswer3.DisabledState.BorderColor = Color.DarkGray;
+            lblAnswer3.DisabledState.CustomBorderColor = Color.DarkGray;
+            lblAnswer3.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            lblAnswer3.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            lblAnswer3.FillColor = Color.Transparent;
+            lblAnswer3.Font = new Font("Lato", 11.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblAnswer3.ForeColor = Color.White;
+            lblAnswer3.Location = new Point(0, 3);
+            lblAnswer3.Name = "lblAnswer3";
+            lblAnswer3.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            lblAnswer3.Size = new Size(493, 34);
+            lblAnswer3.TabIndex = 24;
+            lblAnswer3.Text = "3. How do I uninstall Medusa?";
+            lblAnswer3.TextAlign = HorizontalAlignment.Left;
+            lblAnswer3.Click += lblAnswer3_Click;
             // 
             // FAQ
             // 
@@ -109,12 +207,19 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(44, 44, 44);
             ClientSize = new Size(709, 543);
+            Controls.Add(faqPanel3);
+            Controls.Add(faqPanel2);
             Controls.Add(faqPanel1);
             Controls.Add(label7);
             Name = "FAQ";
             Text = "FAQ";
+            Load += FAQ_Load;
             faqPanel1.ResumeLayout(false);
             faqPanel1.PerformLayout();
+            faqPanel2.ResumeLayout(false);
+            faqPanel2.PerformLayout();
+            faqPanel3.ResumeLayout(false);
+            faqPanel3.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -125,6 +230,11 @@
         private Guna.UI2.WinForms.Guna2Button btnToggle1;
         private Guna.UI2.WinForms.Guna2Panel faqPanel1;
         private Label lblAnswer1;
-        private System.Windows.Forms.Timer faqTimer1;
+        private Guna.UI2.WinForms.Guna2Panel faqPanel2;
+        private Label lblAnswer2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Panel faqPanel3;
+        private Label label1;
+        private Guna.UI2.WinForms.Guna2Button lblAnswer3;
     }
 }
--- FAQ.cs
+++ FAQ.cs
@@ -44,11 +44,56 @@ namespace VirusGuard
                 isExpanded = true;
             }
         }
-        
+
 
         private void lblAnswer1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            if (isExpanded)
+            {
+                // Collapse
+                faqPanel2.Height = collapsedHeight;
+                lblAnswer2.Visible = false;
+                //btnToggle1.Text = "▼ Show Answer";
+                isExpanded = false;
+            }
+            else
+            {
+                // Expand
+                lblAnswer2.Visible = true;
+                faqPanel2.Height = expandedHeight;
+                //btnToggle1.Text = "▲ Hide Answer";
+                isExpanded = true;
+            }
+        }
+
+        private void FAQ_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblAnswer3_Click(object sender, EventArgs e)
+        {
+            if (isExpanded)
+            {
+                // Collapse
+                faqPanel3.Height = collapsedHeight;
+                label1.Visible = false;
+                //btnToggle1.Text = "▼ Show Answer";
+                isExpanded = false;
+            }
+            else
+            {
+                // Expand
+                label1.Visible = true;
+                faqPanel3.Height = expandedHeight;
+                //btnToggle1.Text = "▲ Hide Answer";
+                isExpanded = true;
+            }
         }
     }
 }
